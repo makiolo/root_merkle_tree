391777dbbb97a634729248289133183324eb9843
Ricardo <makiolo@gmail.com>
2022-07-18 00:46:59 +0200

    Create backtesting.py

---
+++ backtesting.py
@@ -0,0 +1,15 @@
+# vectorbt
+
+# https://github.com/polakowo/vectorbt/blob/master/examples/PortfolioOptimization.ipynb
+  
+# riskfolio + vectorbt
+
+# https://nbviewer.org/github/dcajasn/Riskfolio-Lib/blob/master/examples/Tutorial%2018.ipynb
+# https://github.com/dcajasn/Riskfolio-Lib
+# https://riskfolio-lib.readthedocs.io/en/latest/examples.html
+# https://nbviewer.org/github/dcajasn/Riskfolio-Lib/blob/master/examples/Tutorial%201.ipynb
+
+# Financiorencios jupyter (curva libor + ois)
+
+# https://gist.github.com/Financioneroncios/38cef9d2f2f789b281e884e8b33f66a3
+# https://gist.github.com/Financioneroncios?page=2
