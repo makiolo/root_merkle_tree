e610b3c84d1f1cc91bac71034ee426fb55e251da
Ricardo Marmolejo <makiolo@gmail.com>
2022-11-30 12:33:35 +0100

    .

--- gzip_test_encode.py
+++ gzip_test_encode.py
@@ -1,11 +1,14 @@
 import gzip
+import glob
 import base64
-from StringIO import StringIO
+from io import BytesIO
 
-out = StringIO()
-with open('red_pandas.7z', 'rb') as fr:
-    with gzip.GzipFile(fileobj=out, mode="wb") as f:
-        f.write(fr.read())
-message = out.getvalue()
-with open('red_pandas.7z.encoded', 'wb') as f:
-    f.write(base64.b64encode(message))
+for filename in glob.glob('*.7z*'):
+    print('encoding {} ...'.format(filename))
+    out = BytesIO()
+    with open(filename, 'rb') as fr:
+        with gzip.GzipFile(fileobj=out, mode="wb") as f:
+            f.write(fr.read())
+    message = out.getvalue()
+    with open('{}.coded'.format(filename), 'wb') as f:
+        f.write(base64.b64encode(message))
